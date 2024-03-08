67836f5b9525d1726a2265139a3c87e6be11d558
davidf628 <30780186+davidf628@users.noreply.github.com>
2024-03-08 16:53:46 -0500

    not all variables were updating
    fixed an error where not all variables on a single line were updating

--- pytex.py
+++ pytex.py
@@ -67,9 +67,6 @@ eod = 0 # End of TeX document, i.e. the line where \end{document} occurs
 for line_no, line in enumerate(data):
     if re.search(r'\\end\s*\{document\}', line):
         eod = line_no
-        print(f'Line Number: {line_no}')
-
-print(eval('2+2'))
 
 # start reading in the different variables and begin the randomization
 vars = {}
@@ -79,16 +76,16 @@ for i in range(eod+1, len(data)):
         if result:
             vars[result.group(1)] = eval(result.group(2)) # save a record of the variable
             exec(f'{result.group(1)} = {vars[result.group(1)]}') # put the variable and its value into memory
-            print(vars)
+print(vars)
 
 # replace all the locations of the variables within the TeX document
 
 for i in range(0, eod):
-    result = re.search(r'@\s*([A-Za-z_]\w*)\s*@', data[i])
-    if result:
+    while re.search(r'@\s*([A-Za-z_]\w*)\s*@', data[i]):
+        result = re.search(r'@\s*([A-Za-z_]\w*)\s*@', data[i])
         variable_name = result.group(1)
-        print(f'newval == {vars[variable_name]}')
-        data[i] = re.sub(r'@\s*([A-Za-z_]\w*)\s*@', str(vars[variable_name]), data[i])
+        print(f'var == {variable_name} val == {vars[variable_name]}')
+        data[i] = re.sub(r'@\s*' + variable_name + r'\s*@', str(vars[variable_name]), data[i])
         print(data[i])
 
 with open(f'out_{sys.argv[1]}', 'w') as f:
