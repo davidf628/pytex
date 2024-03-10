d3a43cdc236b70ee91bfce8b2ee041bfca499d1a
davidf628 <30780186+davidf628@users.noreply.github.com>
2024-03-10 16:51:02 -0400

    redid everything
    I basically redid everything because arrays were not going to work out now anything between @...@ are considered a python expression and get evaluated

--- lib/misc.py
+++ lib/misc.py
@@ -1,7 +1,79 @@
 import re
 
+def test():
+    test_isNewVariableDec()
+
+def hasPython(s):
+    return re.search(r'\@.*\@', s) != None
+
+def getPython(s):
+    return re.search(r'\@(.*?)\@', s).group(1)
+
+def uncommentLine(s):
+    if re.search(r'^\s*\%\s*', s):
+        return re.search(r'^\s*\%\s*(.*)', s).group(1)
+    else:
+        return s
+
+def strsub(target, value, s):
+    start = s.find(target)
+    end = start + len(target)
+    
+
+def hasVariableCall(s):
+    return re.search(r'\@[A-Za-z_]\w*\@', s) != None
+
+def getVariableName(s):
+    return re.search(r'\@([A-Za-z_]\w*)\@', s).group(1)
+
+def replaceVar(expr, var, val):
+    return re.sub(f'@{var}@', str(val), expr)
+
+
+def hasArrayCall(s):
+    return re.search(r'\@([A-Za-z_]\w*)\[([0-9]*|\@[A-Za-z_]\w*\@)\]\@', s) != None
+
+def getArrayAndIndex(s):
+    result = re.search(r'\@([A-Za-z_]\w*)\[([0-9]*|\@[A-Za-z_]\w*\@)\]\@', s)
+    return [ result.group(1), result.group(2) ]
+
 def subvars(expr, vars):
-    for var_name in vars:
-        if f'@{var_name}@' in expr:
-            expr = re.sub(f'@{var_name}@', str(vars[var_name]), expr)
+    while hasVariableCall(expr):
+        var_name = getVariableName(expr)
+        if var_name in vars:
+            expr = replaceVar(expr, var_name, vars[var_name])
+    while hasArrayCall(expr) or hasVariableCall(expr):
+        if hasVariableCall(expr):
+            pass
+        elif hasArrayCall(expr):
+            var_name, index = getArrayAndIndex(expr)
+            if var_name in vars:
+                pass
+
+    # for var_name in vars:
+    #     if type(vars[var_name]) == str:
+    #         if f'@{var_name}@' in expr:
+    #             expr = re.sub(f'@{var_name}@', str(vars[var_name]), expr)
+    #     elif type(vars[var_name]) == list:
+    #         result = re.search(r'\@([A-Za-z_]\w*)\[([0-9]*|\@[A-Za-z_]\w*\@)\]\@', expr)
+    #         if result:
+                
     return expr
+
+# Returns true if 's' begins with the word: %variables
+def isNewVariableSet(s):
+    return re.search(r'^\s*\%\s*variables\s*$', s, re.IGNORECASE) != None
+
+def isEndVariableSet(s):
+    return re.search(r'^\s*\%\s*end\s*$', s, re.IGNORECASE) != None
+
+def test_isNewVariableDec():
+    print(f'isNewVariableSet("%variables") == {isNewVariableSet("%variables")}')
+    print(f'isNewVariableSet(" %variables") == {isNewVariableSet(" %variables")}')
+    print(f'isNewVariableSet(" % variables") == {isNewVariableSet(" % variables")}')
+    print(f'isNewVariableSet("%VARIABLES") == {isNewVariableSet("%VARIABLES")}')
+    print(f'isNewVariableSet("7%variables") == {isNewVariableSet("7%variables")}')
+    print(f'isNewVariableSet("%variables to do") == {isNewVariableSet("%variables to do")}')
+
+if __name__ == '__main__':
+    test()
--- pytex.py
+++ pytex.py
@@ -15,36 +15,56 @@ with open(filename, "r") as f:
 # read through the document looking for variable declarations
 i = 0
 while (i < len(data)):
-    line = data[i]
 
     if re.search(r'\\newquestion', line):
         __debugvar = 15
 
     # search for variable declarations
-    if re.search(r'\s*^\%\s*variables\s*$', line, re.IGNORECASE):
-        vars = {} # start a new variable "scope"
-        i += 1 # advance to next line
-        while data[i].strip() == '': # skip any blank lines
-            i += 1
-        result = re.search(r'\s*\%\s*([A-Za-z_]\w*)\s*=\s*(.*)', data[i])
-        while result: # the current line depicts a variable declaration
-            expr = subvars(result.group(2), vars)
-            vars[result.group(1)] = eval(expr) # save a record of the variable 
-            i += 1 # advance to next line
-            while data[i].strip() == '': # skip any blank lines
-                i += 1
-            result = re.search(r'\s*\%\s*([A-Za-z_]\w*)\s*=\s*(.*)', data[i])
-        i -= 1       
+    if isNewVariableSet(data[i]):
+
+        data.pop(i)
+        #i += 1 # advance past the variable declaration
+
+        while not isEndVariableSet(data[i]):
+        # remove all the variables from python and start a new variable scope
+        #vars = {}
+
+            command = uncommentLine(data.pop(i))
+
+            exec(command)
+
+        
+        data.pop(i) # remove %end line
+            # advance to next line
+            # while data[i].strip() == '': # skip any blank lines
+            #     i += 1
+            # result = re.search(r'\s*\%\s*([A-Za-z_]\w*)\s*=\s*(.*)', data[i])
+            # while result: # the current line depicts a variable declaration
+
+            #     #expr = subvars(result.group(2), vars)
+            #     #vars[result.group(1)] = eval(expr) # save a record of the variable 
+            #     i += 1 # advance to next line
+            #     while data[i].strip() == '': # skip any blank lines
+            #         i += 1
+            #     result = re.search(r'\s*\%\s*([A-Za-z_]\w*)\s*=\s*(.*)', data[i])
+            # i -= 1       
 
     # if a line is not a variable declaraion, see if it contains any
     #  variables to substitute   
-    else:
-        while re.search(r'@\s*([A-Za-z_]\w*)\s*@', data[i]):
-            result = re.search(r'@\s*([A-Za-z_]\w*)\s*@', data[i])
-            variable_name = result.group(1)
-            data[i] = re.sub(r'@\s*' + variable_name + r'\s*@', str(vars[variable_name]), data[i])
+    elif hasPython(data[i]):
+        while hasPython(data[i]):
+            snippet = getPython(data[i])
+            result = eval(snippet)
+            #data[i] = strsub(snippet, result, data[i])
+            data[i] = re.sub(f'\\@.*?\\@', str(result), data[i], 1)
             print(data[i])
-    i += 1
+        # while re.search(r'@\s*([A-Za-z_]\w*)\s*@', data[i]):
+        #     result = re.search(r'@\s*([A-Za-z_]\w*)\s*@', data[i])
+        #     variable_name = result.group(1)
+        #     data[i] = re.sub(r'@\s*' + variable_name + r'\s*@', str(vars[variable_name]), data[i])
+        #     print(data[i])
+    else:
+        i += 1
 
 with open(f'./output/out_{sys.argv[1]}', 'w') as f:
     for line in data:
